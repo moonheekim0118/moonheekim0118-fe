b336c173bd64aae17d3ccffddddc2c13b7e7a3d1
moonheekim0118 <hellomooneekim@gmail.com>
2023-01-29 04:28:44 +0900

    feat: 로그인 시, accessToken과 userId를 세션스토리지에 저장하도록 구현

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,7 +1,9 @@
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
+
 import { login, LoginParams, LoginResponse } from '../../apiFetchers/auth';
+import { authStore } from './utils';
 
 const useLogin = () => {
   const router = useRouter();
@@ -10,7 +12,8 @@ const useLogin = () => {
     ['login'],
     ({ id, password }) => login({ id, password }),
     {
-      onSuccess: () => {
+      onSuccess: (data) => {
+        authStore.set({ accessToken: data.accessToken, userId: data.user.id });
         router.push('/');
       },
     }
